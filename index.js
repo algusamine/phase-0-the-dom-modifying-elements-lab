4cf549c63c6603da95a28b93d9df17417bf0a6e1
Amine <algusamine@gmail.com>
2021-06-10 18:44:59 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code here!
+main.remove();
+const newHeader = document.createElement('h1');
+newHeader.setAttribute('id','victory');
+document.getElementById('victory');
+newHeader.innerHTML = "<h1>Amine is the champion</h1>"
+
+
